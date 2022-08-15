948948909e11e1afdf5647ebc1b8f9c5d96359a4
David Hernandez Morones <david.2.hernandez@atos.net>
2022-08-15 14:05:16 -0500

    Modelos a usar en la api

---
+++ src/InterviewAPI/Models/Interview.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InterviewAPI.Models
+{
+    public class Interview
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime Appointment { get; set; }
+
+        public Interviewee Interviewee { get; set; }
+        public IEnumerable<Interviewer> Interviewers { get; set; }
+    }
+}
---
+++ src/InterviewAPI/Models/Interviewer.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InterviewAPI.Models
+{
+    public class Interviewer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime BirthDay { get; set; }
+        
+        public List<Interview> Interviews { get; set; }
+        
+    }
+}
